f66784a652d59907618117f301a8b3afab6063f8
Khayyam Saleem <khayyam.saleem@gmail.com>
2017-10-15 15:49:59 -0400

    initial commit

---
+++ hss371_paper1.d
@@ -0,0 +1,37 @@
+#===Dependents for hss371_paper1:
+hss371_paper1.pdf hss371_paper1.d :\
+    /etc/texmf/web2c/texmf.cnf\
+    /usr/share/texlive/texmf-dist/fonts/type1/public/amsfonts/cm/cmr10.pfb\
+    /usr/share/texlive/texmf-dist/fonts/type1/public/amsfonts/cm/cmr7.pfb\
+    /usr/share/texlive/texmf-dist/tex/latex/base/article.cls\
+    /usr/share/texlive/texmf-dist/tex/latex/base/size10.clo\
+    /usr/share/texlive/texmf-dist/web2c/texmf.cnf\
+    /usr/share/texmf/web2c/texmf.cnf\
+    /var/lib/texmf/fonts/map/pdftex/updmap/pdftex.map\
+    /var/lib/texmf/web2c/pdftex/pdflatex.fmt\
+    hss371_paper1.tex
+#===End dependents for hss371_paper1:
+
+#===Phony rules for hss371_paper1:
+
+/etc/texmf/web2c/texmf.cnf :
+
+/usr/share/texlive/texmf-dist/fonts/type1/public/amsfonts/cm/cmr10.pfb :
+
+/usr/share/texlive/texmf-dist/fonts/type1/public/amsfonts/cm/cmr7.pfb :
+
+/usr/share/texlive/texmf-dist/tex/latex/base/article.cls :
+
+/usr/share/texlive/texmf-dist/tex/latex/base/size10.clo :
+
+/usr/share/texlive/texmf-dist/web2c/texmf.cnf :
+
+/usr/share/texmf/web2c/texmf.cnf :
+
+/var/lib/texmf/fonts/map/pdftex/updmap/pdftex.map :
+
+/var/lib/texmf/web2c/pdftex/pdflatex.fmt :
+
+hss371_paper1.tex :
+
+#===End phony rules for hss371_paper1:
